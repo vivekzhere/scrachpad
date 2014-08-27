f534e1dc085d38f68f4ce9e7cd26e71216c93049
Vivek Anand T Kallampally <vivekzhere@gmail.com>
2014-08-27 13:04:12 +0530

    Added comments to LP assignment

--- LinearProgramming/assign1.c
+++ LinearProgramming/assign1.c
@@ -66,13 +66,14 @@ void linear(float A[M][N + 1], int x, int y, float a, float b)
 	}
 }
 
-/* Convert given matrix to row echelon form
+/* Convert given matrix to row echelon form.
  */
 void row_echelon(float A[M][N + 1])
 {
 	int i, j, k;
 	
-	for (i = 0, j = 0; i < M && j <= N; j++) {
+	/* Move diagonally making them 1 */
+	for (i = 0, j = 0; i < M && j < N; i++, j++) {
 
 		/* Find a non zero entry in the jth column from ith row to end
 		 * Mostly the A[i][j] will be non zero. If so the loop
@@ -88,11 +89,16 @@ void row_echelon(float A[M][N + 1])
 		/* If all entries in the jth cloumn is zero skip to next colomn
 		 * without incrementing row index
 		 */
-		if ( k == M )
+		if ( k == M ) {
+			/* do not move to next row */
+			i--;
 			continue;
+		}
 
 		/* Make the j th element of the ith row 1 */
 		scale(A, i, 1/A[i][j]);
+		/* X * (1/X) might be computed as 1 +/- error. Correct it */
+		A[i][j] = 1;
 
 		/* Make entire jth colomn zero except the ith row */
 		for (k = 0; k < M; k++) {
@@ -100,9 +106,6 @@ void row_echelon(float A[M][N + 1])
 				continue;
 			linear(A, k, i, 1, -1 * A[k][j]);
 		}
-
-		/* move to next row */
-		i++;
 	}
 }
 
@@ -111,6 +114,7 @@ void row_echelon(float A[M][N + 1])
  * A and corresponding entry in B is non zero, then the 
  * system has no solution.
  * Returns 1 if a solution exist. Otherwise 0.
+ * Input A should be in row echelon form
  */
 int checkif_sol_exist(float A[M][N + 1])
 {
@@ -118,16 +122,27 @@ int checkif_sol_exist(float A[M][N + 1])
 
 	for (i = 0; i < M; i++) {
 		for (j = 0; j < N && IS_ZERO(A[i][j]); j++);
-		if (j == N && !IS_ZERO(A[i][j]))
+
+		/* If above loop reach j = N, it means all elements in
+		 * the row except the last element is zero. Now check
+		 * if last element is zero. If it is not zero, the
+		 * system is inconsistent
+		 */
+		if (j == N && !IS_ZERO(A[i][N]))
 			return 0;
 	}
+
+	/* The is no row which has all elements zero except the last
+	 * element. So the system is consistent
+	 */
 	return 1;
 }
 
 /* Find a equation with a single variable in the row echelon form
- * (All other variables have zero coefficient
+ * (All other variables have zero coefficient)
  * Returns the index of the first such equation.
  * Returns M if no such equation is found
+ * Input A is assumed to be in row echelon form.
  */
 int find_single_row(float A[M][N + 1])
 {
@@ -166,30 +181,66 @@ void substitute_variable(float A[M][N + 1], int var, float val)
  * is substituted by the RHS of equation.
  * If no such equation is found, substitute the value
  * rand for one unsolved variable
+ * Input A should be in row echelon form.
  */
 void assign_and_solve(float A[M][N + 1], float rand)
 {
-	float X[N];
-	int flag[N] = {0};
+	int flag[N] = {0}; /* Is the variable solved or not */
 	int i, j, k;
+	float variable_val;
 
 	j = 0;
+
+	/* In each iteration of this while loop, exactly one variable will
+	 * get a value assigned. But it need not be the jth variable.
+	 */
 	while (j < N) {
+		/* Find a row with a single variable
+		 */
 		i = find_single_row(A);
+
 		if (i != M) {
+			/* If an equation in a single variable exist, the return
+			 * value of find_single_row i != M. The ith row will
+			 * contain only a single variable and its coeficient is one,
+			 * since our matrix is in row echelon form.
+			 */
+			/* Now find the variable with non zero coeficient in the ith row.
+			 */
 			for (k = 0; k < N && IS_ZERO(A[i][k]); k++);
-			printf("X%d = %.2f\n", k, A[i][N]);
-			substitute_variable(A, k, A[i][N]);
-			i = find_single_row(A);
-		} else {
-			for (k = N - 1; k >= 0 && flag[k] == 1; k--);
-			if (k >= 0) {
-				printf("X%d = %.2f\n", k, rand);
-				substitute_variable(A, k, rand);
-				i = find_single_row(A);
+
+			if (A[i][k] != 1) {
+				/* Something has gone bad. The coeficient of this
+				 * should be 1, beacuse the matrix is in row echelon
+				 * form. The first non zero entry in each row must
+				 * be 1.
+				 */
+				printf("You should not be here!!!\n");
+				return;
 			}
+
+			/* The ith equation has only kth varialble in it and its
+			 * coeficient is one. Also since the matrix is in row echelon
+			 * form this variable will not be part of any other equation.
+			 * The ith equation is Xk = A[i][N]. So we can
+			 * substitute kth variable with A[i][N]
+			 */
+			variable_val = A[i][N];
+		} else {
+			/* find_single_row returns M if all equations have more than
+			 * one variable. So we take the LAST unsolved variable and
+			 * assign value "rand" to it. The variable is taken from last
+			 * so that even after substituting the variable, the row echelon
+			 * form is maintained.
+			 */
+			for (k = N - 1; k >= 0 && flag[k] == 1; k--);
+			variable_val = rand;
 		}
-		flag[k] = 1;
+		flag[k] = 1; /* Mark variable as solved */
+
+		/* Substiute the variable with value in the system */
+		substitute_variable(A, k, variable_val);
+		printf("X%d = %.2f\n", k, variable_val);
 		j++;
 	}
 }
@@ -199,21 +250,32 @@ int main()
 	int i, j;
 	float A[M][N + 1];
 
+	/* Read the M * N matrix A */
 	for (i = 0; i < M; i++)
 		for (j = 0; j < N; j++)
 			scanf ("%f", &A[i][j]);
 
+	/* Read the M * 1 matrix B and augment it to A*/
 	for (i = 0; i < M; i++)
 		scanf ("%f", &A[i][N]);
 
+	/* Convert A to row echelon form */
 	row_echelon(A);
+
+	/* Check whether system is consistent */
 	if (checkif_sol_exist(A) == 0) {
 		printf ("Equation is in consistent\n");
 		return 0;
 	}
 
 	printf("One solution is :\n");
+	/* Solve and priint the solutions */
 	assign_and_solve(A, 1);
+
+	/* Verification print. If every is correct this should print all
+	 * zeros. All variables have been assigned values and substituted.
+	 */
+	printf("\nVerification :\n");
 	print_matrix(A);
 	return 0;
 }
