4d15100458dfc521d2b16176888313c7e798447b
Vivek Anand T Kallampally <vivekzhere@gmail.com>
2015-12-08 11:17:29 +0530

    Implement matching logic for kmp

--- kmp/kmp.cpp
+++ kmp/kmp.cpp
@@ -5,8 +5,11 @@
 using namespace std;
 
 class kmpMatcher {
-	string pattern;
-	vector<int> transissionFn;
+	string pattern; /* Pattern to me matched */
+	vector<int> transissionFn; /* On a match the state of the machine just increments.
+				      But on failure the next state need to calculated so that,
+				      we can make use of the already matched portion of the string.
+				      */
 	
 	public:
 	void printMachine() {
@@ -17,21 +20,43 @@ class kmpMatcher {
 		cout<<endl;
 
 	}
+	/* Pre process the pattern to calulate the next state on fallure for each state,
+	 *
+	 */
 	kmpMatcher (string p) {
-		int i, j;
+		int nextState, currentState;
 		pattern = p;
 		transissionFn.push_back(-1);
-		for (i = -1, j = 0; j < pattern.length(); j++) {
-			while(i >= 0 && pattern[i] != pattern[j])
-				i = transissionFn[i];
-			i++;
-			transissionFn.push_back(i);
+		for (nextState = -1, currentState = 0; currentState < pattern.length(); currentState++) {
+			while(nextState >= 0 && pattern[nextState] != pattern[currentState])
+				nextState = transissionFn[nextState];
+			nextState++;
+			transissionFn.push_back(nextState);
 		}
+	}
+	bool parseForPattern(string s) {
+		int state, i, m;
+		m = pattern.length();
+		state = i = 0;
+		while (i < s.length()) {
+			while (state == m || (state >= 0 && pattern[state] != s[i]))
+				state = transissionFn[state];
+			state++;
+			i++;
+			if (state == m)
+				return true;
+		}
+		return false;
 	}
 
 };
 int main() {
 	kmpMatcher m("abcabcdeab");
 	m.printMachine();
+	if (m.parseForPattern("dggaabcabcabcabcdeabcabc"))
+		cout<<"Match"<<endl;
+	else
+		cout<<"No Match"<<endl;
+
 	return 0;
 }
